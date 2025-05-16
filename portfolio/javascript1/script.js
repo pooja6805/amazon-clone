09c6d272a165b036d5951d9a543de4a18509f6c0
pooja6805 <pujabajjard9876@gmail.com>
2025-05-16 11:20:04 +0530

    api call

--- portfolio/javascript1/script.js
+++ portfolio/javascript1/script.js
@@ -1,7 +1,26 @@
-const URL="https://cat-fact.herokuapp.com/facts";
+const URL="https://meowfacts.herokuapp.com/?count=3";
+const factpara=document.querySelector("#fact");
+const btn=document.querySelector("#btn");
 
-const getfacts = async () =>{
-    console.log("getting data .......");
-    let response = await fetch(URL);
-    console.log(response);
+const getFacts= async()=>{
+   console.log("getting data........");
+    let response= await fetch(URL);
+     console.log(response);
+     let data= await response.json();
+     factpara.innerText= data[0].text();
 };
+btn.addEventListener("click",getFacts);
+// async function fetchTextData() {
+//   try {
+//     const response = await fetch('https://meowfacts.herokuapp.com/?count=3'); // Replace with your URL
+//     const text = await response.text();  // Get plain text from response
+//     console.log(text);  // Use the text data
+//   } catch (error) {
+//     console.error('Error:', error);  // Handle errors
+//   }
+// }
+
+// fetchTextData();
+
+
+
